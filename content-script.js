01dd56866b2960ea0c77aae5563993e44639cfe0
박재성 <qkrwotjd1445@naver.com>
2022-01-19 17:40:08 +0900

    feat: 다른 곳 클릭하면 Hover 창 사라지게 만들고, 특정 글씨를 드래그하면 Hover 창 나타나게 만들기

--- content-script.js
+++ content-script.js
@@ -1,22 +1,27 @@
 const hoverHTML = `
-<div id="hover-container">
-  <select name="folders" id="folders">
-    <option value="toeic">Toeic</option>
-    <option value="toss">Toss</option>
-    <option value="favorite">Favorite</option>
-  </select>
-  <textarea rows="5" cols="33"></textarea>
-</div>`
-const div = document.createElement('div');
-div.innerHTML = hoverHTML;
-document.body.appendChild(div);
+<select name="folders" id="folders">
+  <option value="toeic">Toeic</option>
+  <option value="toss">Toss</option>
+  <option value="favorite">Favorite</option>
+</select>
+<textarea rows="5" cols="33"></textarea>
+`
+const hoverContainer = document.createElement('div');
+hoverContainer.setAttribute("id", "hover-container")
+hoverContainer.innerHTML = hoverHTML;
+document.body.appendChild(hoverContainer);
+console.log("시작")
+hoverContainer.style.display = "none";
+
 
 // 마우스를 눌렀다가 땔 때 발생하는 이벤트
 document.addEventListener('mouseup', function(e) {
+  console.log("mouseup 이벤트")
   const selectedText = window.getSelection().toString();
-  if (selectedText > 0) {
-    
+  const hover = document.getElementById("hover-container");
+  if (selectedText.length > 0) {
+    hover.style.display = "block";
+  } else {
+    hover.style.display = "none";
   }
 })
-
-
--- popup.js
+++ popup.js
@@ -3,7 +3,10 @@ const onOffBtn = document.getElementById("onOffBtn");
 onOffBtn.addEventListener('click', (e) => {
   if (onOffBtn.innerHTML === "ON") {
     onOffBtn.innerHTML = "OFF"
+    chrome.action.enable();
   } else if (onOffBtn.innerHTML === "OFF") {
     onOffBtn.innerHTML = "ON"
+    console.log("disable 함수 실행");
+    chrome.action.disable();
   }
 })
