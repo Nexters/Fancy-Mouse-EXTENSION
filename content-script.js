7e71466ef6ea4d92e2e25c32a8d6c85fbd914dff
박재성 <qkrwotjd1445@naver.com>
2022-01-19 18:17:23 +0900

    fix: Hover 창이 뜨지 않는 버그 수정

--- content-script.js
+++ content-script.js
@@ -1,3 +1,32 @@
+const initHoverContainer = () => {
+  console.log("init")
+  const hoverHTML = `
+<select name="folders" id="folders">
+  <option value="toeic">Toeic</option>
+  <option value="toss">Toss</option>
+  <option value="favorite">Favorite</option>
+</select>
+<textarea rows="5" cols="33"></textarea>
+`
+  const hoverContainer = document.createElement('div');
+  hoverContainer.setAttribute("id", "hover-container")
+  hoverContainer.innerHTML = hoverHTML;
+  document.body.appendChild(hoverContainer);
+  hoverContainer.style.display = "none";
+}
+
+const hide = (element) => {
+  element.style.display = "none";
+}
+
+const show = (element) => {
+  element.style.display = "block";
+}
+
+const getHoverContainer = () => {
+  return document.getElementById("hover-container");
+}
+
 initHoverContainer();
 
 document.addEventListener('mouseup', function(e) {
@@ -9,32 +38,3 @@ document.addEventListener('mouseup', function(e) {
     hide(hoverContainer);
   }
 })
-
-
-const initHoverContainer = () => {
-  const hoverHTML = `
-<select name="folders" id="folders">
-  <option value="toeic">Toeic</option>
-  <option value="toss">Toss</option>
-  <option value="favorite">Favorite</option>
-</select>
-<textarea rows="5" cols="33"></textarea>
-`
-  const hoverContainer = getHoverContainer();
-  hoverContainer.setAttribute("id", "hover-container")
-  hoverContainer.innerHTML = hoverHTML;
-  document.body.appendChild(hoverContainer);
-  hoverContainer.style.display = "none";
-}
-
-const getHoverContainer = () => {
-  return document.getElementById("hover-container");
-}
-
-const hide = (element) => {
-  element.style.display = "none";
-}
-
-const show = (element) => {
-  element.style.display = "block";
-}
