41add93ba3ec465e59259aa44943243c8c392f25
박재성 <qkrwotjd1445@naver.com>
2022-01-21 13:32:24 +0900

    fix: 익스텐션이 작동하지 않는 버그 수정

--- content-script.js
+++ content-script.js
@@ -6,7 +6,11 @@ const initHoverContainer = () => {
   <option value="toss">Toss</option>
   <option value="favorite">Favorite</option>
 </select>
-<textarea rows="5" cols="33"></textarea>
+<textarea id="memo" rows="5" cols="33"></textarea>
+<button id="cancel-btn">취소</button>
+<button id="store-btn">저장</button>
+<button id="config-btn">설정</button>
+<button id="voca-btn">단어장 전체보기</button>
 `
   const hoverContainer = document.createElement('div');
   hoverContainer.setAttribute("id", "hover-container")
@@ -27,13 +31,17 @@ const getHoverContainer = () => {
   return document.getElementById("hover-container");
 }
 
+const getSelectedTextBox = () => {
+  return document.getElementById("selected-text-box");
+}
+
 const showSelectedTextOnHover = (selectedText) => {
-  const selectedTextBox = document.getElementById("selected-text-box");
+  const selectedTextBox = getSelectedTextBox();
   selectedTextBox.innerHTML = selectedText
 }
 
 const hideSelectedTextOnHover = () => {
-  const selectedTextBox = document.getElementById("selected-text-box");
+  const selectedTextBox = getSelectedTextBox();
   selectedTextBox.innerHTML = "";
 }
 
